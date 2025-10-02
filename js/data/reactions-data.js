442ae097f5da89736afec3eee2b2662e75b457a9
Rafsan1711 <samiulhaquerafsan@gmail.com>
2025-10-02 13:16:41 +0600

    Update reactions-data.js

--- js/data/reactions-data.js
+++ js/data/reactions-data.js
@@ -11,42 +11,48 @@ const reactionsDatabase = [
         coefficients: [2, 1],
         products: ['H₂O'],
         productCoefficients: [2],
-        type: 'synthesis'
+        type: 'synthesis',
+        name: 'Water Formation'
     },
     {
         reactants: ['H₂', 'N₂'],
         coefficients: [3, 1],
         products: ['NH₃'],
         productCoefficients: [2],
-        type: 'synthesis'
+        type: 'synthesis',
+        name: 'Ammonia Synthesis'
     },
     {
         reactants: ['C', 'O₂'],
         coefficients: [1, 1],
         products: ['CO₂'],
         productCoefficients: [1],
-        type: 'synthesis'
+        type: 'synthesis',
+        name: 'Carbon Dioxide Formation'
     },
     {
         reactants: ['Mg', 'O₂'],
         coefficients: [2, 1],
         products: ['MgO'],
         productCoefficients: [2],
-        type: 'synthesis'
+        type: 'synthesis',
+        name: 'Magnesium Oxide Formation'
     },
     {
         reactants: ['Fe', 'S'],
         coefficients: [1, 1],
         products: ['FeS'],
         productCoefficients: [1],
-        type: 'synthesis'
+        type: 'synthesis',
+        name: 'Iron Sulfide Formation'
     },
     {
         reactants: ['Na', 'Cl₂'],
         coefficients: [2, 1],
         products: ['NaCl'],
         productCoefficients: [2],
-        type: 'synthesis'
+        type: 'synthesis',
+        name: 'Sodium Chloride Formation'
     },
     
     // Decomposition Reactions
@@ -55,21 +61,24 @@ const reactionsDatabase = [
         coefficients: [2],
         products: ['H₂', 'O₂'],
         productCoefficients: [2, 1],
-        type: 'decomposition'
+        type: 'decomposition',
+        name: 'Water Electrolysis'
     },
     {
         reactants: ['CaCO₃'],
         coefficients: [1],
         products: ['CaO', 'CO₂'],
         productCoefficients: [1, 1],
-        type: 'decomposition'
+        type: 'decomposition',
+        name: 'Calcium Carbonate Decomposition'
     },
     {
         reactants: ['H₂O₂'],
         coefficients: [2],
         products: ['H₂O', 'O₂'],
         productCoefficients: [2, 1],
-        type: 'decomposition'
+        type: 'decomposition',
+        name: 'Hydrogen Peroxide Decomposition'
     },
     
     // Single Displacement Reactions
@@ -78,28 +87,32 @@ const reactionsDatabase = [
         coefficients: [1, 1],
         products: ['Cu', 'FeSO₄'],
         productCoefficients: [1, 1],
-        type: 'single_displacement'
+        type: 'single_displacement',
+        name: 'Copper Displacement'
     },
     {
         reactants: ['AgNO₃', 'Cu'],
         coefficients: [2, 1],
         products: ['Ag', 'Cu(NO₃)₂'],
         productCoefficients: [2, 1],
-        type: 'single_displacement'
+        type: 'single_displacement',
+        name: 'Silver Displacement'
     },
     {
         reactants: ['HCl', 'Zn'],
         coefficients: [2, 1],
         products: ['H₂', 'ZnCl₂'],
         productCoefficients: [1, 1],
-        type: 'single_displacement'
+        type: 'single_displacement',
+        name: 'Hydrogen Gas Formation'
     },
     {
         reactants: ['Cl₂', 'NaBr'],
         coefficients: [1, 2],
         products: ['Br₂', 'NaCl'],
         productCoefficients: [1, 2],
-        type: 'single_displacement'
+        type: 'single_displacement',
+        name: 'Halogen Displacement'
     },
     
     // Combustion Reactions
@@ -108,21 +121,24 @@ const reactionsDatabase = [
         coefficients: [1, 2],
         products: ['CO₂', 'H₂O'],
         productCoefficients: [1, 2],
-        type: 'combustion'
+        type: 'combustion',
+        name: 'Methane Combustion'
     },
     {
         reactants: ['C₂H₆', 'O₂'],
         coefficients: [2, 7],
         products: ['CO₂', 'H₂O'],
         productCoefficients: [4, 6],
-        type: 'combustion'
+        type: 'combustion',
+        name: 'Ethane Combustion'
     },
     {
         reactants: ['C₃H₈', 'O₂'],
         coefficients: [1, 5],
         products: ['CO₂', 'H₂O'],
         productCoefficients: [3, 4],
-        type: 'combustion'
+        type: 'combustion',
+        name: 'Propane Combustion'
     },
     
     // Neutralization Reactions
@@ -131,21 +147,24 @@ const reactionsDatabase = [
         coefficients: [1, 1],
         products: ['H₂O', 'NaCl'],
         productCoefficients: [1, 1],
-        type: 'neutralization'
+        type: 'neutralization',
+        name: 'Acid-Base Neutralization'
     },
     {
         reactants: ['H₂SO₄', 'NaOH'],
         coefficients: [1, 2],
         products: ['H₂O', 'Na₂SO₄'],
         productCoefficients: [2, 1],
-        type: 'neutralization'
+        type: 'neutralization',
+        name: 'Sulfuric Acid Neutralization'
     },
     {
         reactants: ['HNO₃', 'KOH'],
         coefficients: [1, 1],
         products: ['H₂O', 'KNO₃'],
         productCoefficients: [1, 1],
-        type: 'neutralization'
+        type: 'neutralization',
+        name: 'Nitric Acid Neutralization'
     },
     
     // More Common Reactions
@@ -154,70 +173,80 @@ const reactionsDatabase = [
         coefficients: [4, 3],
         products: ['Al₂O₃'],
         productCoefficients: [2],
-        type: 'synthesis'
+        type: 'synthesis',
+        name: 'Aluminum Oxide Formation'
     },
     {
         reactants: ['Ca', 'O₂'],
         coefficients: [2, 1],
         products: ['CaO'],
         productCoefficients: [2],
-        type: 'synthesis'
+        type: 'synthesis',
+        name: 'Calcium Oxide Formation'
     },
     {
         reactants: ['Fe₂O₃', 'H₂'],
         coefficients: [1, 3],
         products: ['Fe', 'H₂O'],
         productCoefficients: [2, 3],
-        type: 'single_displacement'
+        type: 'single_displacement',
+        name: 'Iron Ore Reduction'
     },
     {
         reactants: ['CuO', 'H₂'],
         coefficients: [1, 1],
         products: ['Cu', 'H₂O'],
         productCoefficients: [1, 1],
-        type: 'single_displacement'
+        type: 'single_displacement',
+        name: 'Copper Oxide Reduction'
     },
     {
         reactants: ['AgCl'],
         coefficients: [2],
         products: ['Ag', 'Cl₂'],
         productCoefficients: [2, 1],
-        type: 'decomposition'
+        type: 'decomposition',
+        name: 'Silver Chloride Decomposition'
     },
     {
         reactants: ['C₂H₅OH', 'O₂'],
         coefficients: [1, 3],
         products: ['CO₂', 'H₂O'],
         productCoefficients: [2, 3],
-        type: 'combustion'
+        type: 'combustion',
+        name: 'Ethanol Combustion'
     },
     {
         reactants: ['CO', 'O₂'],
         coefficients: [2, 1],
         products: ['CO₂'],
         productCoefficients: [2],
-        type: 'combustion'
+        type: 'combustion',
+        name: 'Carbon Monoxide Combustion'
     },
     {
         reactants: ['N₂', 'O₂'],
         coefficients: [1, 1],
         products: ['NO'],
         productCoefficients: [2],
-        type: 'synthesis'
+        type: 'synthesis',
+        name: 'Nitric Oxide Formation'
     },
     {
         reactants: ['NO', 'O₂'],
         coefficients: [2, 1],
         products: ['NO₂'],
         productCoefficients: [2],
-        type: 'synthesis'
+        type: 'synthesis',
+        name: 'Nitrogen Dioxide Formation'
     },
     {
         reactants: ['SO₂', 'O₂'],
         coefficients: [2, 1],
         products: ['SO₃'],
         productCoefficients: [2],
-        type: 'synthesis'
+        type: 'synthesis',
+        name: 'Sulfur Trioxide Formation'
     }
 ];
 
@@ -253,4 +282,4 @@ function getAllReactants() {
     });
     
     return Array.from(reactantsSet).sort();
-      }
+        }
